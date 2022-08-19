4cd3d45b4c06d25d3c55a58dacb6778c74682d95
unknown <buragohain4@gmail.com>
2022-08-20 01:48:59 +0530

    Adding Question 6

---
+++ Q6/Q6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace Q6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           string hashingNumbersFilePath = @"C:\Users\Amit Pegu\Desktop\DataStructures\Q6\HashingInputNumbers.txt";
+            bool isRun = true;
+            while (isRun)
+            {
+                Console.WriteLine("\nSelect and enter program number:" +
+                    "(6. Hashing function)");
+                int programOption = Convert.ToInt32(Console.ReadLine());
+                switch (programOption)
+                {
+                    case 6:
+                        int[] numbersList = Array.ConvertAll(File.ReadAllText(hashingNumbersFilePath).Split(","), int.Parse);
+                        // Array.Sort(numbersList);
+                        new HashingFunction().Hashing(numbersList);
+                        break;
+                    default:
+                        isRun = !isRun;
+                        break;
+                }
+            }
+
+        }
+    }
+}
